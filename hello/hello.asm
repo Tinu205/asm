4a5733077fc7b9c27ec51bb0f8978b1e0c63061b
Tinu205 <dazleroger404@gmail.com>
2024-11-17 12:38:15 +0530

    Added hello files

---
+++ hello/hello.asm
@@ -0,0 +1,20 @@
+.global _start
+.intel_syntax
+.section .text
+
+_start:
+  mov %eax,4
+  mov %ebx,1
+  lea %ecx,[message]
+  mov %edx,13
+  int 0x80
+
+  #exit syscall
+  mov %eax,1
+  mov %ebx,65
+  int 0x80
+
+
+.section .data
+  message:
+  .ascii "Hello, world\n"
