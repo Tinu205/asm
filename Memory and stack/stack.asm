f520e16a42e71fc8f1a8b0d3c668164ffdcb91c4
Tinu205 <dazleroger404@gmail.com>
2024-11-19 18:44:26 +0530

    Memory and stack is added

---
+++ Memory and stack/stack.asm
@@ -0,0 +1,22 @@
+.global _start
+.intel_syntax
+
+
+.section .text
+
+_start:
+
+  ;mov dword ptr[%esp],0x0a434241
+  push 0x0a434241
+  mov %eax,4
+  mov %ebx,1
+  mov %ecx,%esp
+  mov %edx,4
+  int 0x80
+
+
+  mov %eax,1
+  mov %ebx,69
+  int 0x80
+
+ 
