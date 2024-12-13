4e2c0290652d6084d217404f4cd65be15f708791
Tinu205 <dazleroger404@gmail.com>
2024-12-13 10:47:19 +0530

    fixed the string lenght problem -> added .data about .text

--- Calculator/calculator.asm
+++ Calculator/calculator.asm
@@ -2,6 +2,17 @@
 .intel_syntax noprefix 
 
 
+
+.section .data
+  first_num : .ascii "Enter first number: \n"
+  f_len = . - first_num
+  second_num: .ascii "Enter second number: \n"
+  s_len = . - second_num
+  operation : .ascii "Enter the operation to be performed: \n"
+  op_len = . - operation
+
+ 
+
 .section .text
 _start:
   mov eax, 4
@@ -9,21 +20,28 @@ _start:
   lea ecx,[first_num]
   mov edx, f_len
   int 0x80
+  
+
+  mov eax, 4
+  mov ebx, 1
+  lea ecx,[second_num]
+  mov edx, s_len
+  int 0x80
+
+
+  mov eax, 4
+  mov ebx, 1
+  lea ecx,[operation]
+  mov edx, op_len
+  int 0x80
+
 
 
   #exit code
   mov eax,1
   mov ebx,0
   int 0x80
-
-
-.section .data
-  first_num : .ascii "Enter first number: \n"
-  f_len = . - first_num
-  second_num: .ascii "Enter second number: \n"
-  operation : .ascii "Enter the operation to be performed: \n"
-
-  
+ 
 .section .bss
 
 
